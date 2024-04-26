077e68567b1305918a1d311c83f1f4428e974709
taylous <taylous99@gmail.com>
2024-04-26 16:54:34 +0900

    initiate project

---
+++ src/main/java/PrimeFactor.java
@@ -0,0 +1,5 @@
+public class PrimeFactor {
+
+    public PrimeFactor() {
+    }
+}
---
+++ src/test/java/PrimeFactorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PrimeFactorTest {
+
+    @Test
+    void createDefaultConstructor() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(0, 0);
+    }
+}
