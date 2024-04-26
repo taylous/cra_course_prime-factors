3129f6286741a56e58c2019b674174bdbaf2b1e8
taylous <taylous99@gmail.com>
2024-04-26 17:14:18 +0900

    [Add] tdd 7cycle

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactor {
 
     public PrimeFactor() {
+    }
+
+    public List<Integer> of(int number) {
+        List<Integer> factors = new ArrayList<>();
+
+        for (int divisor = 2; number > 1; divisor += 1) {
+            while (number % divisor == 0) {
+                factors.add(divisor);
+                number /= divisor;
+            }
+        }
+        
+        return factors;
     }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,12 +1,58 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PrimeFactorTest {
 
+    private PrimeFactor primeFactor;
+
+    @BeforeEach
+    void setUp() {
+        this.primeFactor = new PrimeFactor();
+    }
+
     @Test
-    void createDefaultConstructor() {
-        PrimeFactor primeFactor = new PrimeFactor();
-        assertEquals(0, 0);
+    void testPrimeFactorOf1() {
+        this.primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(), primeFactor.of(1));
+    }
+
+    @Test
+    void testPrimeFactorOf2() {
+        this.primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(2), primeFactor.of(2));
+    }
+
+    @Test
+    void testPrimeFactorOf3() {
+        this.primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(3), primeFactor.of(3));
+    }
+
+    @Test
+    void testPrimeFactorOf4() {
+        this.primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(2, 2), primeFactor.of(4));
+    }
+
+    @Test
+    void testPrimeFactorOf6() {
+        this.primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(2, 3), primeFactor.of(6));
+    }
+
+    @Test
+    void testPrimeFactorOf9() {
+        this.primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(3, 3), primeFactor.of(9));
+    }
+
+    @Test
+    void testPrimeFactorOf12() {
+        this.primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(2, 2, 3), primeFactor.of(12));
     }
 }
